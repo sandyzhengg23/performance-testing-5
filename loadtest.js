d350f14ba88018809e6d42ef47e2fd6f56c4127a
sandyzhengg <128326330+sandyzhengg23@users.noreply.github.com>
2026-05-07 21:38:52 -0500

    added load and spike tests + screenshots

---
+++ loadtest.js
@@ -0,0 +1,29 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '30s', target: 10 },
+    { duration: '1m', target: 25 },
+    { duration: '30s', target: 0 },
+  ],
+
+  thresholds: {
+    http_req_duration: ['p(95)<500'],
+    http_req_failed: ['rate<0.01'],
+    http_reqs: ['rate>5'],
+  },
+};
+
+export default function () {
+  const res = http.get('https://quickpizza.grafana.com/');
+
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
+
+
+
